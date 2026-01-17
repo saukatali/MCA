86ae7303e8d3e2340067eb070196d03b59685180
Saukatali <masisokatali@gmail.com>
2026-01-17 17:58:21 +0530

    Add files via upload

---
+++ SEM -1/c programming/big_value.c
@@ -0,0 +1,18 @@
+//find biggest value between two numbers
+#include <stdio.h>
+
+int main() {
+    int a,b;
+    printf("Enter one value of A:\n");
+    scanf("%d",&a);
+    printf("Enter second value of B: \n");
+    scanf("%d",&b);
+    
+    if(a >b){
+        printf("a is biggrr: %d \n",a);
+    }else{
+        printf("b is bigger: %d \n",b);
+    }
+
+    return 0;
+}
