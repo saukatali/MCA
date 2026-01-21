f222c084dd468cb1a823fa57c26fb167658a5ec1
Saukatali <masisokatali@gmail.com>
2026-01-21 14:20:35 +0530

    Add files via upload

---
+++ SEM -1/c programming/21-ArraySortOrNot.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int isSort(int[], int);
+void main() {
+    int arr[10];
+    int n, i;
+
+    printf("Enter an n : ");
+    scanf("%d", &n);
+    printf("Enter an array elements : \n");
+    for(i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+    int result = isSort(arr, n);
+    if(result) {
+        printf("Array is sorted\n");
+    } else {
+        printf("Array is not sorted\n");
+    }
+}
+
+int isSort(int arr[], int n) {
+    int i;
+    for(i = 0; i < n-1; i++) {
+        if(arr[i] >= arr[i+1]){
+            return 0;
+        }
+    }
+    return 1;
+}
