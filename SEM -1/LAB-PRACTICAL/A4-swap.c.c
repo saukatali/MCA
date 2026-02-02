14bd26d224c88b4c632474012988e805ff8b7e54
Saukatali <masisokatali@gmail.com>
2026-02-02 20:54:46 +0530

    Implement swap functions using value and reference
    Added functions to swap numbers by value and by reference.

--- SEM -1/LAB-PRACTICAL/A4-swap.c.c
+++ SEM -1/LAB-PRACTICAL/A4-swap.c.c
@@ -1,18 +1,31 @@
-// Online C compiler to run C program online
+// Program to swap two numbers using call by value and call by reference
 #include <stdio.h>
+#include <conio.h>
+void swapbyvalue(int, int);
+void swapByReference(int *, int *);
 
-
-void main() {
-    
-   int a,b,temp;
-   
-   printf("Enter a,b \n");
-   scanf("%d %d",&a,&b);
-   
-   temp = a;
-   a = b;
-   b = temp;
-   
-   printf("a = %d b = %d \n", a , b);
-    
+int main() {
+    int a, b;
+    printf("Enter two numbers: ");
+    scanf("%d %d", &a, &b);
+    printf("Before swapping: a = %d, b = %d\n", a, b);
+    swapbyvalue(a, b);
+    printf("After swapping in main function: a = %d, b = %d\n", a, b);
+    swapByReference(&a, &b);
+    printf("After swapping in main function: a = %d, b = %d\n", a, b);
+    return 0;
+}
+void swapbyvalue(int a, int b) {
+    int temp;
+    temp = a;
+    a = b;
+    b = temp;
+    printf("After swapping in swap function: a = %d, b = %d\n", a, b);
+}
+void swapByReference(int *a, int *b) {
+    int temp;
+    temp = *a;
+    *a = *b;
+    *b = temp;
+    printf("After swapping in swapByReference function: a = %d, b = %d\n", *a, *b);
 }
