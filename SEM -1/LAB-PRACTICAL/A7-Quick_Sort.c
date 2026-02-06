0f7566e2c74962b6309857b08d704cd8f9042ee6
Saukatali <masisokatali@gmail.com>
2026-02-06 20:24:05 +0530

    Update A7-Quick_Sort.c

--- SEM -1/LAB-PRACTICAL/A7-Quick_Sort.c
+++ SEM -1/LAB-PRACTICAL/A7-Quick_Sort.c
@@ -2,41 +2,42 @@
 
 int partition(int arr[], int low, int high);
 
-void quicksort(int arr[], int low, int high)
-{
+void quicksort(int arr[], int low, int high){
     int pos;
-    if (low < high)
-    {
+    if (low < high){
         pos = partition(arr, low, high);
         quicksort(arr, low, pos - 1);
         quicksort(arr, pos + 1, high);
     }
 }
 
-int partition(int arr[], int low, int high)
-{
-    int pivot, i, j, temp;
+int partition(int arr[], int low, int high){
+    int pivot = arr[low];
+    int i = low + 1;
+    int j = high;
+    int temp;
 
-    pivot = arr[low];
-    i = low + 1;
-    j = high;
+    // Loop to find the correct position for pivot
+    for (;;) {
+        // Move i to the right while arr[i] <= pivot
+        for (; i <= high && arr[i] <= pivot; i++)
+            ;
 
-    while (i <= j)
-    {
-        while (i <= high && arr[i] <= pivot)
-            i++;
+        // Move j to the left while arr[j] > pivot
+        for (; arr[j] > pivot; j--)
+            ;
 
-        while (arr[j] > pivot)
-            j--;
-
-        if (i < j)
-        {
+        // If i < j, swap elements
+        if (i < j) {
             temp = arr[i];
             arr[i] = arr[j];
             arr[j] = temp;
+        } else {
+            break;  // Exit the loop if i >= j
         }
     }
 
+    // Move pivot to the correct position
     temp = arr[low];
     arr[low] = arr[j];
     arr[j] = temp;
