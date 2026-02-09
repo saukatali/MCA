223e52c143048c60745db53030cb0342b2b8452e
Saukatali <masisokatali@gmail.com>
2026-02-09 22:59:59 +0530

    Fix formatting and clean up swap program

--- SEM -1/LAB-PRACTICAL/A4-Swap.c.c
+++ SEM -1/LAB-PRACTICAL/A4-Swap.c.c
@@ -1,20 +1,6 @@
 // Program to swap two numbers using call by value and call by reference
 #include <stdio.h>
 #include <conio.h>
-void swapbyvalue(int, int);
-void swapByReference(int *, int *);
-
-int main() {
-    int a, b;
-    printf("Enter two numbers: ");
-    scanf("%d %d", &a, &b);
-    printf("Before swapping: a = %d, b = %d\n", a, b);
-    swapbyvalue(a, b);
-    printf("After swapping in main function: a = %d, b = %d\n", a, b);
-    swapByReference(&a, &b);
-    printf("After swapping in main function: a = %d, b = %d\n", a, b);
-    return 0;
-}
 void swapbyvalue(int a, int b) {
     int temp;
     temp = a;
@@ -29,3 +15,17 @@ void swapByReference(int *a, int *b) {
     *b = temp;
     printf("After swapping in swapByReference function: a = %d, b = %d\n", *a, *b);
 }
+
+
+int main() {
+    int a, b;
+    printf("Enter two numbers: ");
+    scanf("%d %d", &a, &b);
+    printf("Before swapping: a = %d, b = %d\n", a, b);
+    swapbyvalue(a, b);
+    printf("After swapping in main function: a = %d, b = %d\n", a, b);
+    swapByReference(&a, &b);
+    printf("After swapping in main function: a = %d, b = %d\n", a, b);
+    return 0;
+}
+
